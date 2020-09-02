22302cec4c734ca9b19a4465fbcb3e6ebff5ac05
Andres Orbe <aeo34@njit.edu>
2020-09-02 18:33:57 -0400

    single code comment

--- go.js
+++ go.js
@@ -1,7 +1,6 @@
 browser.browserAction.onClicked.addListener((_) => {
     browser.tabs.executeScript({
-        code: 'let vid = this.document.getElementsByTagName("video")[0];' +
-        'vid.playbackRate = 16.00;'
+        code: 'this.document.getElementsByTagName("video")[0].playbackRate = 15.00;'
     })
     console.log("script has been run");
 });
